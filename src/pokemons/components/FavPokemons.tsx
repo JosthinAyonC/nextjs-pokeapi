794d2e0aed129019ae97e217a95795a7979562fb
Chustin <josthinayon@gmail.com>
2024-05-20 15:18:33 -0500

    Versiones favoritos finalizada

--- src/pokemons/components/FavPokemons.tsx
+++ src/pokemons/components/FavPokemons.tsx
@@ -8,15 +8,17 @@ export const FavPokemons = () => {
     const favPokemons = useAppSelector((state) => state.favPokemons.pokemons);
 
     return (
-        <div className="flex flex-wrap gap-10 items-center justify-center m-2">
-            <h1 className="text-7xl">Lista de favoritos</h1>
-            {
-                favPokemons.map((pokemon) => (
-                    <PokemonCard
-                        key={pokemon.id}
-                        pokemon={pokemon} />
-                ))
-            }
+        <div className="flex flex-col m-2">
+            <h1 className="text-7xl text-center">Lista de favoritos</h1>
+            <div className="flex flex-wrap gap-10 items-center justify-center m-2">
+                {
+                    favPokemons.map((pokemon) => (
+                        <PokemonCard
+                            key={pokemon.id}
+                            pokemon={pokemon} />
+                    ))
+                }
+            </div>
         </div>
     )
 }
